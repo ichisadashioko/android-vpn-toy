9a054522b8fec30ac792bc0cd706ceb5d8228646
shioko <ichisadashioko@gmail.com>
2021-06-03 15:50:45 +0000

    format java code

--- app/src/main/java/io/github/ichisadashioko/android/vpn/toy/MainActivity.java
+++ app/src/main/java/io/github/ichisadashioko/android/vpn/toy/MainActivity.java
@@ -1,8 +1,8 @@
 package io.github.ichisadashioko.android.vpn.toy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
